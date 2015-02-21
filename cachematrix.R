60c981e9bbce1bc5033fec0f7605b3afa0d5d71c
victor-chang <vchang@yahoo.com>
2015-02-21 09:51:24 -0700

    Revert "implement function"
    This reverts commit 72a95860bf750a9a316cc54f5a3cb2815d92cd07.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This functin create a special "matrix" object that
-## caches its inverse
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    
-    setInverse <- function(inverse) inv <<- inverse
-    getInverse <- function() inv
-    list(set = set, get=get, setInverse = setInverse,
-         getInverse = getInverse)
-        
+
 }
 
 
-## This function computes the inverse of the special "matrix" retruned by 
-## makeCacheMatrix. If the inverse has already been calculated (and the 
-## matrix has not changed), then the cachesolve should retrieve the inverse
-## from the cache.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    inv <- x$getInverse()
-    
-    if (!is.null(inv)) {
-        message("getting cached data")
-        return (inv)
-    }
-    
-    data <- x$get()
-    inv <- solve(data)
-    x$setInverse(inv)
-    
-    inv
-        
+        ## Return a matrix that is the inverse of 'x'
 }
